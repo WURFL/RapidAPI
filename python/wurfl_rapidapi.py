aed861e5a2507c8499165f8af9b128e0fc29eb6c
andrea <castello.a@gmail.com>
2021-04-01 10:31:56 +0200

    Improved rapidapi python example (loads json requests from file and sends them)

--- python/wurfl_rapidapi.py
+++ python/wurfl_rapidapi.py
@@ -9,31 +9,44 @@ except KeyError as ke:
     print('Error: you must set RAPIDAPI_KEY and RAPIDAPI_HOST environment variables to use this code sample')
     exit(1)
 
-conn = http.client.HTTPSConnection(rapidapi_host)
+# read json requests from file
+try:
+    requests_file = open('../requests.json', 'r')
+    request_lines = requests_file.readlines()
+except FileNotFoundError as e:
+    print('Error, requests file not found: ' + str(e))
+    exit(1)
 
-# This payload selects only three capabilities (1 static and two virtual), you can select all the exposed capabilities
-# by removing the two requested* arrays (also remember to remove the comma after the curly brace,
-# or you'll get an error 500
-payload = '''
-{
-  "lookup_headers": {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; ASUS_Z017D Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/539.36",
-	"content-Type": "application/json",
-    "x-requested-with":"XMLHttpRequest"
-  },
-  "requested_caps": ["is_tablet"],
-  "requested_vcaps": ["complete_device_name", "form_factor"]     
-}
-'''
+# each line contains a set of HTTP headers taken from real requests a saved in json format
+for line in request_lines:
+    conn = http.client.HTTPSConnection(rapidapi_host)
+    json_string = line.replace("'", "\"")
+    # we get the headers as a dictionary
+    headers = json.loads(json_string)
+    # Create the payload to send to our request.
+    # This payload selects only three capabilities (1 static and two virtual), you can select all the exposed
+    # capabilities by removing the two requested* arrays as show in the commented line below
+    post_body = {"lookup_headers": headers, "requested_caps": ["is_tablet"], "requested_vcaps": ["complete_device_name",
+                                                                                                 "form_factor"]}
+    # use this line if you want to get all capabilities exposed by the API
+    # post_body = {"lookup_headers": headers }
+    payload = json.dumps(post_body)
 
-headers = {
-    'x-rapidapi-host': rapidapi_host,
-    'x-rapidapi-key': rapidapi_key,
-    'content-type': "application/json"
-}
+    # set the headers for our request. Be aware that these headers are different from the headers of the request
+    # used to  build the POST body
+    headers = {
+        'x-rapidapi-host': rapidapi_host,
+        'x-rapidapi-key': rapidapi_key,
+        'content-type': "application/json"
+    }
 
-conn.request("POST", "/v2/lookuprequest/json", payload, headers)
+    try:
+        conn.request("POST", "/v2/lookuprequest/json", payload, headers)
+        res = conn.getresponse()
+        data = json.loads(res.read())
+        print(json.dumps(data, indent=4, sort_keys=True))
+    except Exception as e:
+        print('Something went wrong: ' + str(e))
+        exit(1)
 
-res = conn.getresponse()
-data = json.loads(res.read())
-print(json.dumps(data, indent=4, sort_keys=True))
+print('RapidApi WURFL Detection script end')
